54487c23d1a4e8b71791799c0d7b331a6da6d975
Rand-Abdulaziz <randabdulazizsa@gmail.com>
2025-01-02 00:05:54 +0300

    Add project files.

---
+++ WMS/Dashboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMS
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+        String role;
+        public Dashboard(String role)
+        {
+            InitializeComponent();
+            this.role = role;
+            Console.WriteLine("what  ? Role: " + role);
+        }
+    }
+}
---
+++ WMS/DatabaseOperations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMS
+{
+    internal class DatabaseOperations
+    {
+        protected SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source=warehousedb.chce80egkzdn.eu-north-1.rds.amazonaws.com;initial catalog=WHMSdb;user id=admin1;password=TheStrongTeam1;";
+
+            return con;
+        }
+
+        public DataSet getData(String query)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlConnection con = getConnection();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = query;
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in getData : " + ex);
+            }
+            return ds;
+        }
+
+        public void setData(String query, String msg)
+        {
+            try
+            {
+                SqlConnection con = getConnection();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                con.Open();
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+                con.Close();
+                if (msg != null)
+                {
+                    MessageBox.Show(msg, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+    }
+}
